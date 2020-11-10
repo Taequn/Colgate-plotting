d8298e7c18af2dd207dd58f4700cf18f6caac99a
Kirill Chernyakov <taequn@gmail.com>
2020-11-10 15:36:46 -0500

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,138 @@
+import requests
+import re
+import json
+import oldDB as db
+
+#capacity for classes
+#Neil Albert
+
+department_dict = {
+	"ALST": "Africana and Latin American Studies",
+	"ANTH": "Anthropology",
+	"ARTS": "Art and Art History",
+	"AHUM": "Arts and Humanities",
+	"ASIA": "Asian Studies",
+	"ASTR": "Astronomy",
+	"BIOL": "Biology",
+	"CHEM": "Chemistry",
+	"CHIN": "Chinese",
+	"CLAS": "The Classics",
+	"COSC": "Computer Science",
+	"ECON": "Economics",
+	"EDUC": "Educational Studies",
+	"ENGL": "English",
+	"ENST": "Environmental Studies",
+	"FMST": "Film and Media Studies",
+	"FREN": "French",
+	"GEOG": "Geography",
+	"GEOL": "Geology",
+	"GERM": "German",
+	"GREK": "Greek",
+	"HEBR": "Hebrew",
+	"HIST": "History",
+	"ITAL": "Italian",
+	"JAPN": "Japanese",
+	"JWST": "Jewish Studies",
+	"LATN": "Latin",
+	"LGBT": "Lesbian, Gay, Bisexual, Transgender, and Queer Studies",
+	"CORE": "CORE studies",
+	"LING": "Linguistics",
+	"MATH": "Mathematics",
+	"ARAB": "Middle Eastern and Islamic Studies (Language)",
+	"MIST": "Middle Eastern and Islamic Studies",
+	"MUSE": "Museum Studies",
+	"MUSI": "Music",
+	"NAST": "Native American Studies",
+	"NASC": "Natural Science",
+	"NEUR": "Neuroscience",
+	"PCON": "Peace and Conflict Studies",
+	"PHIL": "Philosophy",
+	"PHYS": "Physics",
+	"POSC": "Political Science",
+	"PSYC": "Psychology",
+	"RELG": "Religion",
+	"REST": "Russian and Eurasian Studies",
+	"SOSC": "Social Sciences",
+	"SOCI": "Sociology",
+	"SPAN": "Spanish",
+	"THEA": "Theater",
+	"UNST": "University Studies",
+	"WMST": "Women’s Studies",
+	"WRIT": "Writing and Rhetoric",
+	"FSEM": "First year seminar",
+	"HUMN": "Test",
+	"LCTL": "Less Commonly Taught Languages"
+}
+
+semester_links=["https://api.colgate.edu/v1/courses/search?keyword=&termCode=201501&coreArea=&inquiryArea=&meetTimeMorning=&meetTimeAfternoon=&meetTimeEvening=&openCoursesOnly=",
+"https://api.colgate.edu/v1/courses/search?keyword=&termCode=201502&coreArea=&inquiryArea=&meetTimeMorning=&meetTimeAfternoon=&meetTimeEvening=&openCoursesOnly=",
+"https://api.colgate.edu/v1/courses/search?keyword=&termCode=201601&coreArea=&inquiryArea=&meetTimeMorning=&meetTimeAfternoon=&meetTimeEvening=&openCoursesOnly=",
+"https://api.colgate.edu/v1/courses/search?keyword=&termCode=201602&coreArea=&inquiryArea=&meetTimeMorning=&meetTimeAfternoon=&meetTimeEvening=&openCoursesOnly=",
+"https://api.colgate.edu/v1/courses/search?keyword=&termCode=201701&coreArea=&inquiryArea=&meetTimeMorning=&meetTimeAfternoon=&meetTimeEvening=&openCoursesOnly=",
+"https://api.colgate.edu/v1/courses/search?keyword=&termCode=201702&coreArea=&inquiryArea=&meetTimeMorning=&meetTimeAfternoon=&meetTimeEvening=&openCoursesOnly=",
+"https://api.colgate.edu/v1/courses/search?keyword=&termCode=201801&coreArea=&inquiryArea=&meetTimeMorning=&meetTimeAfternoon=&meetTimeEvening=&openCoursesOnly=",
+"https://api.colgate.edu/v1/courses/search?keyword=&termCode=201802&coreArea=&inquiryArea=&meetTimeMorning=&meetTimeAfternoon=&meetTimeEvening=&openCoursesOnly=",
+"https://api.colgate.edu/v1/courses/search?keyword=&termCode=201901&coreArea=&inquiryArea=&meetTimeMorning=&meetTimeAfternoon=&meetTimeEvening=&openCoursesOnly=",
+"https://api.colgate.edu/v1/courses/search?keyword=&termCode=201902&coreArea=&inquiryArea=&meetTimeMorning=&meetTimeAfternoon=&meetTimeEvening=&openCoursesOnly=",
+"https://api.colgate.edu/v1/courses/search?keyword=&termCode=202001&coreArea=&inquiryArea=&meetTimeMorning=&meetTimeAfternoon=&meetTimeEvening=&openCoursesOnly=",
+"https://api.colgate.edu/v1/courses/search?keyword=&termCode=202002&coreArea=&inquiryArea=&meetTimeMorning=&meetTimeAfternoon=&meetTimeEvening=&openCoursesOnly=",
+]
+
+json_arr = []
+
+"""
+for link in range(len(semester_links)):
+	page = requests.get(semester_links[link])
+	result_dict = json.loads(page.text)
+	counting_dict = {}
+
+	for x in department_dict:
+		counting_dict[x]=0
+
+	for x in result_dict:
+		counting_dict[x["DISPLAY_KEY"][:4]]+=1
+
+	counting_dict["Year"]=int(result_dict[0]["TERM_CODE"][:4])
+	counting_dict["Semester"]=int(result_dict[0]["TERM_CODE"][-1])
+
+	json_arr.append(sorted(counting_dict.items(), key=lambda x: x[1], reverse=True))
+
+	print("Year: %s, %s semester" % (result_dict[0]["TERM_CODE"][:4], result_dict[0]["TERM_CODE"][-1]))
+	print(sorted(counting_dict.items(), key=lambda x: x[1], reverse=True))
+	print()
+"""
+#with open("data.json", "w") as outfile:
+#	json.dump(json_arr, outfile)
+
+
+#useless code — too lazy to remove
+page = requests.get(semester_links[-2])
+result_dict = json.loads(page.text)
+counting_dict = {}
+
+for x in department_dict:
+	counting_dict[x]=0
+
+for x in result_dict:
+	counting_dict[x["DISPLAY_KEY"][:4]]+=1
+
+
+#print("Fall 2020:\n")
+#print(sorted(counting_dict.items(), key=lambda x: x[1], reverse=True))
+#print()
+
+db.printingValues("200402.json")
+db.printingValues("201402.json")
+
+
+#Some useless crap for now
+#print("Term code: " + x["TERM_CODE"])
+#print("CRN: " + x["CRN"])
+#print("Key: " + x["DISPLAY_KEY"][:-2])
+#print("Department: " + department_dict[x["DISPLAY_KEY"][:4]])
+#print("Title: " + x["TITLE"])
+#print("Status: " + x["STATUS"])
+#print("Seats: " + x["SEATS"] + "\n")
+
+
+
---
+++ oldDB.py
@@ -0,0 +1,136 @@
+import requests
+import re
+import json
+
+#capacity for classes
+#Neil Albert
+
+department_dict = {
+	"ALST": "Africana and Latin American Studies",
+	"ANTH": "Anthropology",
+	"ARTS": "Art and Art History",
+	"AHUM": "Arts and Humanities",
+	"ASIA": "Asian Studies",
+	"ASTR": "Astronomy",
+	"BIOL": "Biology",
+	"CHEM": "Chemistry",
+	"CHIN": "Chinese",
+	"CLAS": "The Classics",
+	"COSC": "Computer Science",
+	"ECON": "Economics",
+	"EDUC": "Educational Studies",
+	"ENGL": "English",
+	"ENST": "Environmental Studies",
+	"FMST": "Film and Media Studies",
+	"FREN": "French",
+	"GEOG": "Geography",
+	"GEOL": "Geology",
+	"GERM": "German",
+	"GREK": "Greek",
+	"HEBR": "Hebrew",
+	"HIST": "History",
+	"ITAL": "Italian",
+	"JAPN": "Japanese",
+	"JWST": "Jewish Studies",
+	"LATN": "Latin",
+	"LGBT": "Lesbian, Gay, Bisexual, Transgender, and Queer Studies",
+	"CORE": "CORE studies",
+	"LING": "Linguistics",
+	"MATH": "Mathematics",
+	"ARAB": "Middle Eastern and Islamic Studies (Language)",
+	"MIST": "Middle Eastern and Islamic Studies",
+	"MUSE": "Museum Studies",
+	"MUSI": "Music",
+	"NAST": "Native American Studies",
+	"NASC": "Natural Science",
+	"NEUR": "Neuroscience",
+	"PCON": "Peace and Conflict Studies",
+	"PHIL": "Philosophy",
+	"PHYS": "Physics",
+	"POSC": "Political Science",
+	"PSYC": "Psychology",
+	"RELG": "Religion",
+	"REST": "Russian and Eurasian Studies",
+	"SOSC": "Social Sciences",
+	"SOCI": "Sociology",
+	"SPAN": "Spanish",
+	"THEA": "Theater",
+	"UNST": "University Studies",
+	"WMST": "Women’s Studies",
+	"WRIT": "Writing and Rhetoric",
+	"FSEM": "First year seminar",
+	"HUMN": "Test",
+	"LCTL": "Less Commonly Taught Languages",
+	"RUSS": "Russian",
+	"SOAN": "Sociology and Anthropology",
+	"ROLA": "Romance Languages and Literatures"
+}
+
+def printingValues(name):
+	test_reading = json.loads(open("Database/"+name, "r").read())
+
+	counting_dict = {}
+
+	for x in department_dict:
+		counting_dict[x]=0
+
+	for x in test_reading['data']:
+		counting_dict[x["subject"]]+=1
+
+	print(test_reading['data'][0]['termDesc']+"\n")
+	#print("Total classes: " + str(test_reading['totalCount']))
+	print(sorted(counting_dict.items(), key=lambda x: x[1], reverse=True))
+
+"""
+for link in range(len(semester_links)):
+	page = requests.get(semester_links[link])
+	result_dict = json.loads(page.text)
+	counting_dict = {}
+
+	for x in department_dict:
+		counting_dict[x]=0
+
+	for x in result_dict:
+		counting_dict[x["DISPLAY_KEY"][:4]]+=1
+
+	counting_dict["Year"]=int(result_dict[0]["TERM_CODE"][:4])
+	counting_dict["Semester"]=int(result_dict[0]["TERM_CODE"][-1])
+
+	json_arr.append(sorted(counting_dict.items(), key=lambda x: x[1], reverse=True))
+
+	print("Year: %s, %s semester" % (result_dict[0]["TERM_CODE"][:4], result_dict[0]["TERM_CODE"][-1]))
+	print(sorted(counting_dict.items(), key=lambda x: x[1], reverse=True))
+	print()
+
+#with open("data.json", "w") as outfile:
+#	json.dump(json_arr, outfile)
+"""
+
+#useless code — too lazy to remove
+"""
+result_dict = json.loads(page.text)
+counting_dict = {}
+
+for x in department_dict:
+	counting_dict[x]=0
+
+for x in result_dict:
+	counting_dict[x["DISPLAY_KEY"][:4]]+=1
+"""
+
+#print("Spring 2021:\n")
+#print(sorted(counting_dict.items(), key=lambda x: x[1], reverse=True))
+
+
+
+#Some useless crap for now
+#print("Term code: " + x["TERM_CODE"])
+#print("CRN: " + x["CRN"])
+#print("Key: " + x["DISPLAY_KEY"][:-2])
+#print("Department: " + department_dict[x["DISPLAY_KEY"][:4]])
+#print("Title: " + x["TITLE"])
+#print("Status: " + x["STATUS"])
+#print("Seats: " + x["SEATS"] + "\n")
+
+
+
