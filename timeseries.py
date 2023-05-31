72c6534f71d731d4d08d3e59c9b075c2df904a8f
Chris Cherniakov <taequn@gmail.com>
2023-05-31 23:48:21 +0500

    Update timeseries.py

--- timeseries.py
+++ timeseries.py
@@ -2,7 +2,15 @@ from plotnine import *
 import pandas as pd
 
 
-
+def term_to_datetime(term):
+    season, year = term.split(' ')
+    if season == 'Spring':
+        month = '01'
+    elif season == 'Summer':
+        month = '07'
+    else:  # Fall
+        month = '12'
+    return pd.to_datetime(f'{year}-{month}-01')
 
 def make_timeseries(data, selected_subjects):
     filtered_data = data[data['subject'].isin(selected_subjects)]
@@ -12,14 +20,23 @@ def make_timeseries(data, selected_subjects):
         filtered_data['subject'] = filtered_data['subject'].iloc[:, 0]
 
     counts = filtered_data.groupby(['term', 'subject']).size().reset_index(name='counts')
-
-    p = (
-        ggplot(counts, aes(x='term', y='counts', color='subject', group='subject')) +
-        geom_line() +
-        scale_x_discrete(limits=sorted(counts['term'].unique())) +
-        theme_bw() +
-        theme(axis_text_x=element_text(rotation=90, hjust=1))
+    counts['term_date'] = counts['term'].apply(term_to_datetime)
+    counts['term'] = pd.Categorical(
+        counts['term'],
+        categories=counts.sort_values('term_date')['term'].unique(),
+        ordered=True
     )
+
+    # Plot data
+    p = (
+        ggplot(counts) +
+        aes(x='term', y='counts', group='subject', color='subject') +
+        geom_line() + 
+        theme_bw() +
+        theme(axis_text_x=element_text(angle=45))  # Rotate x-axis labels
+    )
+
+    
     return p
 
 if __name__ == "__main__":
